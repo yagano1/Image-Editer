0182423efc0107bb7269cec4a9b3573b1478eaae
Linh1991 <mr.yagamiraito@gmail.com>
2016-06-20 13:45:22 +0700

    refomart code

--- app/src/main/java/editer/minhnhan/in/imageediter/MainActivity.java
+++ app/src/main/java/editer/minhnhan/in/imageediter/MainActivity.java
@@ -2,55 +2,34 @@ package editer.minhnhan.in.imageediter;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-
-import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
-
+import android.widget.Spinner;
+import java.io.File;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    public ArrayList<String> images;
-
-    private ArrayList<String> getAllShownImagesPath() {
-        Uri uri;
-        Cursor cursor;
-        int column_index_data;
-        ArrayList<String> listOfAllImages = new ArrayList<String>();
-        String absolutePathOfImage = null;
-        uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-        String[] projection = {MediaStore.MediaColumns.DATA,
-                MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
-        cursor = this.getContentResolver().query(uri, projection, null,
-                null, null);
-        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
-        while (cursor.moveToNext()) {
-            absolutePathOfImage = cursor.getString(column_index_data);
-            listOfAllImages.add(absolutePathOfImage);
-        }
-        return listOfAllImages;
-    }
-
-
+    public ArrayList<String> images = new ArrayList<String>();;
+    public ArrayList<String> album  = new ArrayList<String>();;
+    String path = Environment.getExternalStorageDirectory() + "/Pictures";
+    GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        GridView gridView = (GridView) findViewById(R.id.gridview);
-        images = getAllShownImagesPath();
-        Uri uri = Uri.parse("android.resource://editer.minhnhan.in.imageediter/drawable/camera");
-        images.add(uri.toString());
-        gridView.setAdapter(new ImageListAdapter(this, images));
+        getSupportActionBar().setTitle(null);
+        getListAlbum(path);
+        gridView = (GridView) findViewById(R.id.gridview);
+        album.add("a");
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -67,9 +46,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        getResources().openRawResource(+R.drawable.camera).toString();
-    }
+        final Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, album);
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0)
+                {
+                    getListAlbum(path);
+                    gridView.setAdapter(new ImageListAdapter(MainActivity.this, images));
+                }
+                else {
+                    getListAlbum(path + "/" + dropdown.getItemAtPosition(position));
 
+                    gridView.setAdapter(new ImageListAdapter(MainActivity.this, images));
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -80,7 +81,22 @@ public class MainActivity extends AppCompatActivity {
                 }
         }
 
-
+   public void getListAlbum(String path)
+    {
+        images.clear();
+        ArrayList<String> listOfAllAlbum = new ArrayList<String>();
+        File f = new File(path);
+        File[] files = f.listFiles();
+        for (File inFile : files) {
+            if (inFile.isDirectory()) {
+            }
+            else {
+                images.add(inFile.getAbsolutePath());
+            }
+        }
+        Uri uri = Uri.parse("android.resource://editer.minhnhan.in.imageediter/drawable/camera");
+        images.add(uri.toString());
+    }
 }
 
 
